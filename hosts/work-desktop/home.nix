97953256aa4f8c71d2827a26ccbf20392a47f3ac
andrewkim <akim7@anduril.com>
2026-05-11 09:35:18 -0700

    Update neovim-config flake input

---
+++ hosts/work-desktop/home.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+{
+  home.username = "akim7";
+  home.homeDirectory = "/home/akim7";
+
+  programs.ghostty.enable = pkgs.lib.mkForce false;
+
+  xdg.configFile."ghostty/config".text = ''
+    theme = Dracula
+    command = ${pkgs.zsh}/bin/zsh
+  '';
+
+  xdg.configFile."btop/btop.conf" = {
+    source = ../../config/btop/btop.conf;
+    force = true;
+  };
+
+  xdg.configFile."btop/themes" = {
+    source = ../../config/btop/themes;
+    recursive = true;
+  };
+
+  home.packages = [
+    pkgs.awscli2
+    pkgs.cachix
+  ];
+}
