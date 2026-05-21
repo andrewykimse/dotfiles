1a7bd2e64f09dcd0d3a24bc11996207dff39b180
andrewkim <andrewykimse@gmail.com>
2026-05-20 21:48:37 -0700

    updated desktop monitor scale rule

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -6,6 +6,11 @@
     "XDG_DATA_DIRS,${config.home.homeDirectory}/.nix-profile/share:/usr/local/share:/usr/share"
   ];
 
+  wayland.windowManager.hyprland.settings.monitor = pkgs.lib.mkForce [
+    "desc:Apple Computer Inc StudioDisplay, 5120x2880@60, auto, 2"
+    ", preferred, auto, 1"
+  ];
+
   home.username = "andrewkim";
   home.homeDirectory = "/home/andrewkim";
 
