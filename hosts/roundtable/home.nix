22a078b0fb238f5d07a98ec54f2b6ec213bd5516
Andrew Kim <andrewykimse@gmail.com>
2026-07-15 21:38:51 -0700

    update roundtable

--- hosts/roundtable/home.nix
+++ hosts/roundtable/home.nix
@@ -1,15 +1,12 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
-    ../../modules/shell.nix
-    ../../modules/git.nix
-    ../../modules/terminal.nix
-    ../../modules/dev.nix
+    ../../modules/common.nix
   ];
 
   home.username = "andrewkim";
   home.homeDirectory = "/home/andrewkim";
-  home.stateVersion = "25.11";
+  home.stateVersion = lib.mkForce "25.11";
 
   programs.home-manager.enable = true;
 }
