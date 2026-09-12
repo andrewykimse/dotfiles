0f33e0ad57d163f5122381dd8b2cd5c538571d50
Andrew Kim <andrewykimse@gmail.com>
2026-09-11 21:20:37 -0700

    add signal app

--- modules/common.nix
+++ modules/common.nix
@@ -7,7 +7,12 @@
     ./dev.nix
   ];
 
-  home.packages = [ pkgs.nh pkgs.iperf3 pkgs.spotify-player ];
+  home.packages = [ 
+    pkgs.nh 
+    pkgs.iperf3 
+    pkgs.spotify-player 
+    pkgs.signal-desktop
+  ];
 
   # home.username and home.homeDirectory are set per-host
 
